3944c97c803f5c1bb8044de746f9da48062ff80e
Lyondealpha-hub <75757307+Lyondealpha-hub@users.noreply.github.com>
2022-11-17 09:07:15 +0000

    Add files via upload

--- src/Pages/User.tsx
+++ src/Pages/User.tsx
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from "react";
 import { itemsInterface } from '../Interfaces/interface';
 import axios from "axios";
 import SearchModal from "../components/searchModal";
+import Search from "../components/search";
+import { useDispatch } from "react-redux";
+import { Products_div } from "../features/productshook";
 
 
 
@@ -15,18 +18,22 @@ const User = () =>{
     
 
     const [products, setProducts] = useState<itemsInterface[]>([])
+    const dispatch = useDispatch()
+     const [itemCate, setitemCate] = useState<string>('')
+
 
     useEffect(()=>{
         axios.get("http://localhost:3001/products").
         then((response)=>{
             if(response){
                 setProducts(response.data)
+                // console.log(response.data);
+                //  dispatch(Products_div(response.data))
                 console.log(response.data);
                 
             }
-        })
-
-        console.log(products);
+            
+        })    
         
     },[])
 
@@ -37,19 +44,36 @@ const User = () =>{
     //     {title: 'Rexona', description: 'body splash', price : 25,},
     //     {title: 'boxers', description :'men wear', price: 35}
 
-    // ])
+    // ])'
+
+    
+    
 
     return(
+        
         <div className="w-full h-full">
+            
             <Navbar products={products} />
             
-            <LandingPage />
+            <LandingPage  />
 
             {/* I will work on this more later */}
             {/* Items overview */}
             <div className="h-screen w-full   ">
+                {/* <div className="flex justify-evenly mt-8 flex-wrap ">
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                    <button className='flex items-center  rounded-none h-14 justify-center border-2 w-1/6 ml-4 bg-gray-400 md:rounded-full hover:bg-sky-500 cursor-pointer' type='button'>Featured</button>
+                </div> */}
+               
+
             {/* header */}
             <h1 className="py-10 text-5xl">Store Overview </h1>
+            
                 <div className="w-full justify-evenly flex flex-row h-full flex-wrap">
                     
                     {products.map((lists:itemsInterface, key:number)=>{
--- src/features/Carthook.ts
+++ src/features/Carthook.ts
@@ -1,25 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { mainState } from '../Interfaces/interface';
+import { itemsInterface, mainState } from '../Interfaces/interface';
 import Swal from 'sweetalert2';
+import { MdAirlineSeatFlatAngled } from 'react-icons/md';
 
 
 
 
 // defining my initial state
 const initialState : mainState = {
-    notification : 0,
-    Orderitem : [],
+    notification : JSON.parse(localStorage.getItem('cart_data')!) ?  JSON.parse(localStorage.getItem('cart_data')!).length :  0,
+    Orderitem : !JSON.parse(localStorage.getItem('cart_data')!) ? [] : JSON.parse(localStorage.getItem('cart_data')!),
 }
 
 // delete an item object
 
-const handleDelete = (old_state:any, cart_del:any)=>{
+const handleDelete = (old_state:itemsInterface[], cart_del:any)=>{
+    console.log("you messed with the wrong dude "+cart_del[0]);
     
     for(let deleted = 0; deleted < old_state.length; deleted++){
         let temparr:any = []
         if([old_state[deleted]].findIndex !== cart_del.id){
 
             console.log([old_state[deleted]]);
+            console.log("asd" + cart_del);
+            
             
            return temparr.push(old_state[deleted])
             
@@ -35,9 +39,10 @@ const handleDelete = (old_state:any, cart_del:any)=>{
 
 // checking for duplicate data 
 
-const checker = (old_state:any, cart_new:any)=>{
+const checker = (old_state:itemsInterface[], cart_new:itemsInterface)=>{
     // looping through the initial array
-
+    console.log(cart_new);
+    
     for(var duplicate = 0; duplicate < old_state.length; duplicate++){
         
         if([old_state[duplicate]][0].id === cart_new.id){
@@ -74,12 +79,14 @@ export const cartSlice = createSlice({
                     footer: '<a href="">Why do I have this issue?</a>'
                   }) 
             }else{
+                // if the item does not exist push it into the array 
                 console.log('Added');
                 state.Orderitem.push(item.payload)
+                localStorage.setItem('cart_data', JSON.stringify(state.Orderitem))
 
                 // incrementing the cart icon by number of items
                 state.notification += 1
-
+                
                 
                 Swal.fire({
                     position: 'top',
@@ -100,20 +107,25 @@ export const cartSlice = createSlice({
         },
         deleteItem : (state, item) =>{
 
-            console.log("dellll");
+            
             
             
               var arrupdate =  handleDelete(JSON.parse(JSON.stringify(state.Orderitem)),item.payload);
             
-                
+                    // console.log("dellll " + arrupdate);
                     if (arrupdate == true) {
-                        state.Orderitem = [arrupdate] 
+                        state.Orderitem = [arrupdate]
+                        localStorage.setItem('cart_data', JSON.stringify(state.Orderitem))
                       Swal.fire(
                         'Deleted!',
                         'Your Cart item has been deleted.',
                         'success'
                       )
                       state.notification -= 1
+                      
+                      if(state.notification < 0){
+                    state.notification = 0
+                }
                     }
                 
                   
