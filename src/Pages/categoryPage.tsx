0ab5353dbda6889072b445ab9aa7d77238f3b35f
Lyondealpha-hub <75757307+Lyondealpha-hub@users.noreply.github.com>
2022-11-18 17:54:07 +0000

    Add files via upload

--- src/Pages/categoryPage.tsx
+++ src/Pages/categoryPage.tsx
@@ -1,41 +1,39 @@
-import React,{useState, useEffect} from 'react'
-import { itemsInterface } from '../Interfaces/interface';
+import React, { useState, useEffect } from "react";
+import { itemsInterface } from "../Interfaces/interface";
 import Navbar from "../components/navbar";
-import axios from 'axios'
-import Search from '../components/search';
-import Category from '../components/categories';
+import axios from "axios";
+import Search from "../components/search";
+import Category from "../components/categories";
+import ReactPaginate from "react-paginate";
 
+const CategoryPage = () => {
+  const [products, setProducts] = useState<itemsInterface[]>([]);
 
-const CategoryPage = ()=>{
+  useEffect(() => {
+    axios.get("http://localhost:3001/products").then((response) => {
+      if (response) {
+        setProducts(response.data);
+        // console.log(response.data);
+        //  dispatch(Products_div(response.data))
+        // console.log(response.data);
+      }
+    });
+  }, []);
 
-    const [products, setProducts] = useState<itemsInterface[]>([])
-
-
-
-        useEffect(()=>{
-        axios.get("http://localhost:3001/products").
-        then((response)=>{
-            if(response){
-                setProducts(response.data)
-                // console.log(response.data);
-                //  dispatch(Products_div(response.data))
-                console.log(response.data);
-                
-            }
-            
-        })    
-        
-    },[])
-
-    return(
-        <div>
-            {/* <Navbar /> */}
-            <Navbar products={products} />
-            <div className='flex '>
-                <Category />
-            </div>
+  return (
+    <div className="">
+      {/* <Navbar /> */}
+      <div>
+        <Navbar products={products} />
+      </div>
+      <div className="block">
+        <div className="flex w-full h-full">
+          <Category />
         </div>
-    )
-}
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 export default CategoryPage;
